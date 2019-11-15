4284be301188abbcac33014ec56293049a757e31
SheilaBirgen <jeronobergen@gmail.com>
2019-11-15 09:19:02 +0300

    change

--- js/main.js
+++ js/main.js
@@ -1,42 +1,42 @@
 
-  
-  //feedback form 
 
-  $("#contact").submit(function(event) {
-    event.preventDefault();
-    var blanks = ["name", "email", "message"];
-    var input = [];
-    blanks.forEach(function(blank) {
-      input.push($("#" + blank).val());
-    });
-    alert(
-      "Thank you for reaching out to us. Your message has been received."
-    );
-    $("#contact")[0].reset();
+//feedback form 
+
+$("#contact").submit(function (event) {
+  event.preventDefault();
+  var blanks = ["name", "email", "message"];
+  var input = [];
+  blanks.forEach(function (blank) {
+    input.push($("#" + blank).val());
   });
-  
-  //dark mode
+  alert(
+    "Thank you for reaching out to us. Your message has been received."
+  );
+  $("#contact")[0].reset();
+});
 
-  $( ".inner-switch" ).on("click", function() {
-    if( $( "body" ).hasClass( "dark-mode" )) {
-      $( "body" ).removeClass( "dark-mode" );
-      $( ".inner-switch" ).text( "OFF" );
-    } else {
-      $( "body" ).addClass( "dark-mode" );
-      $( ".inner-switch" ).text( "ON" );
-    }
+//dark mode
+
+$(".inner-switch").on("click", function () {
+  if ($("body").hasClass("dark-mode")) {
+    $("body").removeClass("dark-mode");
+    $(".inner-switch").text("OFF");
+  } else {
+    $("body").addClass("dark-mode");
+    $(".inner-switch").text("ON");
+  }
 });
 // button details2
 
-$(document).ready(function() {
+$(document).ready(function () {
 
-$("#details").click(function() {
-  $(".details1").show();
-});
-$("#details2").click(function() {
-  $(".details2").show();
-});
-$("#details3").click(function() {
-  $(".details3").show();
-});
+  $("#details").click(function () {
+    $(".details1").show();
+  });
+  $("#details2").click(function () {
+    $(".details2").show();
+  });
+  $("#details3").click(function () {
+    $(".details3").show();
+  });
 });
